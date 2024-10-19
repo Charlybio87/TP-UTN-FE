9218b176c7039bd1b453c730a85957d866eec691
Carlos A. Ribas <ribascarlos.a@outlook.com>
2024-10-19 14:54:37 -0300

    Ajustando Responsive web: 2000 y phone: 320

--- src/fetching/productsFetching.js
+++ src/fetching/productsFetching.js
@@ -1,5 +1,5 @@
 const solicitarProductos = async () => {
-  const URL_API_PRODUCTS = 'http://localhost:5173/data/products.json' /*'https://tp-final-front-end.vercel.app/data/products.json'*/ // Ver donde se esta ejecutando
+  const URL_API_PRODUCTS =  'https://tp-final-front-end.vercel.app/data/products.json' // Ver donde se esta ejecutando /*'http://localhost:5173/data/products.json'*/
 
   const respuestaHttp = await fetch(URL_API_PRODUCTS,
       {
